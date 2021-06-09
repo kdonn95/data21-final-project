5cf0180f5d309c37c1e39fea73c5f2cb1a3524a4
David <davidchilds6@gmail.com>
2021-06-09 11:01:58 +0100

    hand over

--- app/jsonextract.py
+++ app/jsonextract.py
@@ -1,17 +1,25 @@
-import boto3
 import json
-import pandas as pd
-
-s3 = boto3.client('s3')
+from pprint import pprint
+import re
+import boto3
 
 bucket_name = 'data21-final-project'
-
-
 s3_resource = boto3.resource('s3')
-bucket = s3.resource.Bucket(bucket_name)
+bucket = s3_resource.Bucket(bucket_name)
 
-# download file into current directory
-for s3_object in bucket.objects.all():
-    if s3_object.endswith(".json"):
-        pass
 
+def extract_json():
+    for s3_object in bucket.objects.all():
+        if s3_object.endswith(".json", ):
+            contents = s3_object["Body"].read()
+            return json.loads(contents)
+        else:
+            raise Exception("JSON files not found.")
+
+
+
+
+bucket = bucket.get_bucket(bucket_name)
+
+for key in bucket.list():
+    print(key.name.encode('utf-8'))
