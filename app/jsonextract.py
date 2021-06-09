6b51a3f7a9a773057898f4dad3464672f834fdf8
Lewis <ltwelftree@spartaglobal.com>
2021-06-09 16:19:53 +0100

    D21FP-6 day 1

--- app/jsonextract.py
+++ app/jsonextract.py
@@ -1,46 +1,57 @@
 import json
-from pprint import pprint
-import re
 import boto3
+import pandas as pd
 
+#init s3
 bucket_name = 'data21-final-project'
 s3_resource = boto3.resource('s3')
 s3_client = boto3.client('s3')
 bucket = s3_resource.Bucket(bucket_name)
 
 
-def return_bucket_json_body(bucket_object):
-    obj = s3_client.get_object(Bucket=bucket_object.bucket_name,
-                               Key=bucket_object.key)
+def return_bucket_json_body(bucket_name, key):
+    """takes bucket_name and key
+    returs json row of body"""
+    obj = s3_client.get_object(Bucket=bucket_name, Key=key)
     contents = obj["Body"].read().decode('utf8')
     return json.loads(contents)
 
 
-def bucket_json_finder():
+def get_bucket_json_key_list():
+    """returns a list of pages(another list) of tuples (bucket_name, key)
+    page size set using chunk"""
+    keylist = []
     for bucket_object in bucket.objects.all():
         if str(bucket_object.key[-5:]) == '.json':
-            yield return_bucket_json_body(bucket_object)
+            keylist.append((bucket_object.bucket_name, bucket_object.key))
+    outlist = []
+    for i in range(0, len(keylist), 300):
+        chunk = keylist[i:i + 300]
+        outlist.append(chunk)
+    return outlist
 
 
-def rows_to_list():
-    for i in bucket_json_finder():
-        print(i)
+def json_file_to_dataframe(keylist):
+    """takes a list of tuples (bucket_name, key)
+    outputs appended dataframe of all rows of files from within list"""
+    rows_df = pd.DataFrame(columns=['name', 'date', 'tech_self_score',
+                                    'strengths', 'weaknesses',
+                                    'self_development', 'geo_flex',
+                                    'financial_support_self', 'result',
+                                    'course_interest'])
+    for tuple in keylist:
+        row = return_bucket_json_body(tuple[0], tuple[1])
+        rows_df = rows_df.append(row, ignore_index=True)
+    return rows_df
 
-rows_to_list()
+
+def yield_pages():
+    """yields each dataframe from s3 pages
+    gets list of pages of keys
+    creates a dataframe per page"""
+    for page in get_bucket_json_key_list():
+        yield json_file_to_dataframe(page)
 
 
-
-def get_txt_file_key_list(self, bucket_name):
-    # Extract all objects in the bucket specified by the user.
-    files = self.s3_resource.Bucket(bucket_name).objects.all()
-
-    # Creating an empty list to store the text file objects.
-    txt_files = []
-
-    # Looking through every object in the bucket and getting only the txt files.
-    for file in files:
-        if ".txt" == file.key[-4:]:
-            txt_files.append(file.key)
-    return txt_files
-
-#pandas SQL
+print(type(next(yield_pages())))
+#sum(len(i) for i in get_bucket_json_key_list())
