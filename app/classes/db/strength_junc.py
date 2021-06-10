0f07bc64683e8a0c045617981b0a4f2de1c9c828
Giacomo Allen <72930861+jagman014@users.noreply.github.com>
2021-06-10 11:13:23 +0100

    Update strength_junc.py

--- app/classes/db/strength_junc.py
+++ app/classes/db/strength_junc.py
@@ -2,11 +2,11 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from app.classes.db.modelbase import SqlAlchemyBase
 
-
-class Strength_junc(SqlAlchemyBase):
+# creation of strength_junc table model
+class StrenghtJunc(SqlAlchemyBase):
     __tablename__ = 'strength_junc'
 
-    strength_id = Column(Integer, ForeignKey('strengths.strength_id'))
-    candidate_id = Column(Integer, ForeignKey('candidate.candidate_id'))
-
-
+    strength_id = Column(Integer,
+                        ForeignKey('strengths.strength_id'), primary_key=True)
+    candidate_id = Column(Integer,
+                        ForeignKey('candidate.candidate_id'), primary_key=True)
