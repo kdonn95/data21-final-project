b4641130c8f39b8980b0f59cfc6ab3b96d47b538
Lewis <ltwelftree@spartaglobal.com>
2021-06-10 11:06:25 +0100

    d21fp-6 tests complete, some files refactored

--- app/classes/jsonextract.py
+++ app/classes/jsonextract.py
@@ -1,16 +1,54 @@
-import boto3
 import json
-import pandas as pdb
+import boto3
+import pandas as pd
+
+class JsonExtract():
+    def __init__(self):
+        self.bucket_name = 'data21-final-project'
+        self.s3_resource = boto3.resource('s3')
+        self.s3_client = boto3.client('s3')
+        self.bucket = self.s3_resource.Bucket(self.bucket_name)
 
 
-s3=boto3.client('s3')
-list=s3.list_objects(Bucket='s3://data21-final-project/')['Contents']
-for s3_key in list:
-    s3_object = s3_key['Key']
-    if not s3_object.endswith("/"):
-        if s3_object.endswith(".json"):
-            s3.download_file('bucket', s3_object, s3_object)
-    else:
-        import os
-        if not os.path.exists(s3_object):
-            os.makedirs(s3_object)
+    def return_bucket_json_body(self, key):
+        """takes bucket_name and key
+        returs json row of body"""
+        obj = self.s3_client.get_object(Bucket=self.bucket_name, Key=key)
+        contents = obj["Body"].read().decode('utf8')
+        return json.loads(contents)
+
+
+    def get_bucket_json_key_list(self):
+        """returns a list of pages(another list) of tuples (bucket_name, key)
+        page size set using chunk"""
+        keylist = []
+        for bucket_object in self.bucket.objects.all():
+            if str(bucket_object.key[-5:]) == '.json':
+                keylist.append((bucket_object.bucket_name, bucket_object.key))
+        outlist = []
+        for i in range(0, len(keylist), 300):
+            chunk = keylist[i:i + 300]
+            outlist.append(chunk)
+        return outlist
+
+
+    def json_file_to_dataframe(self, keylist):
+        """takes a list of tuples (bucket_name, key)
+        outputs appended dataframe of all rows of files from within list"""
+        rows_df = pd.DataFrame(columns=['name', 'date', 'tech_self_score',
+                                        'strengths', 'weaknesses',
+                                        'self_development', 'geo_flex',
+                                        'financial_support_self', 'result',
+                                        'course_interest'])
+        for tuple in keylist:
+            row = self.return_bucket_json_body(tuple[1])
+            rows_df = rows_df.append(row, ignore_index=True)
+        return rows_df
+
+
+    def yield_pages(self):
+        """yields each dataframe from s3 pages
+        gets list of pages of keys
+        creates a dataframe per page"""
+        for page in self.get_bucket_json_key_list():
+            yield self.json_file_to_dataframe(page)
--- unittests/test_jsonextract.py
+++ unittests/test_jsonextract.py
@@ -1,25 +1,71 @@
-from app.classes import jsonextract
+from app.classes.jsonextract import JsonExtract
+import pandas as pd
 
-je = jsonextract()
-
-
-def test_num_of_keys():
-    total = 0
-    for x in range[0 : len(someotherfunction)]:
-        total += len(someotherfunction[x])
-    assert total == 3105
+je = JsonExtract()
 
 
-def test_num_of_pages():
-    assert len(someotherfunction) == 11
+def test_return_bucket_json_body():
+    #je.bucket_name = 'data21-final-project'
+    body = je.return_bucket_json_body('Talent/10383.json')
+    assert body == {'name': 'Stillmann Castano',
+                    'date': '22/08/2019',
+                    'tech_self_score': {'C#': 6,
+                                        'Java': 5,
+                                        'R': 2,
+                                        'JavaScript': 2},
+                    'strengths': ['Charisma'],
+                    'weaknesses': ['Distracted', 'Impulsive', 'Introverted'],
+                    'self_development': 'Yes',
+                    'geo_flex': 'Yes',
+                    'financial_support_self': 'Yes',
+                    'result': 'Pass',
+                    'course_interest': 'Business'}
 
 
-def test_key_types():
-    for key in keylist:
-        assert isinstance(key, DataFrame)
+def test_get_bucket_json_key_list():
+    keylist = je.get_bucket_json_key_list()
+    assert isinstance(keylist, list)
+    assert len(keylist) == 11
+    assert sum([len(i) for i in keylist]) == 3105
 
 
-return_bucket_json_body(self, key):
-get_bucket_json_key_list(self):
-json_file_to_dataframe(self, keylist):
-yield_pages
+def test_yield_pages():
+   first_page = next(je.yield_pages())
+   assert len(first_page) == 300
+   assert isinstance(first_page.iloc[0], pd.core.series.Series)
+   assert all(first_page.iloc[0] == pd.Series({'name': 'Stillmann Castano',
+                    'date': '22/08/2019',
+                    'tech_self_score': {'C#': 6,
+                                        'Java': 5,
+                                        'R': 2,
+                                        'JavaScript': 2},
+                    'strengths': ['Charisma'],
+                    'weaknesses': ['Distracted', 'Impulsive', 'Introverted'],
+                    'self_development': 'Yes',
+                    'geo_flex': 'Yes',
+                    'financial_support_self': 'Yes',
+                    'result': 'Pass',
+                    'course_interest': 'Business'}))
+
+
+def test_json_file_to_dataframe():
+    df = je.json_file_to_dataframe([('data21-final-project',
+                                  'Talent/10383.json')])
+    assert all(df.columns == ['name', 'date', 'tech_self_score',
+               'strengths', 'weaknesses',
+               'self_development', 'geo_flex',
+               'financial_support_self', 'result',
+               'course_interest'])
+    assert all(df.iloc[0] == pd.Series({'name': 'Stillmann Castano',
+                    'date': '22/08/2019',
+                    'tech_self_score': {'C#': 6,
+                                        'Java': 5,
+                                        'R': 2,
+                                        'JavaScript': 2},
+                    'strengths': ['Charisma'],
+                    'weaknesses': ['Distracted', 'Impulsive', 'Introverted'],
+                    'self_development': 'Yes',
+                    'geo_flex': 'Yes',
+                    'financial_support_self': 'Yes',
+                    'result': 'Pass',
+                    'course_interest': 'Business'}))
