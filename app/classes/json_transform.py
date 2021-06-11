9c2e9375c1c7428450e9be8ac8cb67ab7b062f4e
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-06-11 14:28:55 +0100

    json_transform

--- app/classes/json_transform.py
+++ app/classes/json_transform.py
@@ -1,17 +1,14 @@
 from app.classes.json_extract import JsonExtract
 from tabulate import tabulate
-from datetime import datetime
 import datetime
-import pandas as pd
 
-# je = JsonExtract([])
-# page1_df = next(je.yield_pages())
+je = JsonExtract([])
+page1_df = next(je.yield_pages())
 
 
 class JsonTransform:
     def __init__(self):
         self.je = JsonExtract([])
-
 
     def to_bool(self, field):
         field = field.lower()
@@ -22,22 +19,21 @@ class JsonTransform:
         else:
             pass
 
-
     def clean_text(self, text):
-        return text.title().replace("'", "")
-
+        return text.replace("'", "").title()
 
     def convert_date(self, value):
         return datetime.date(int(value[6:11]), int(value[3:5]), int(value[0:2]))
-        #datetime.strptime(value, '%d/%m/%Y').isoformat()[:-9].
 
 
+jt = JsonTransform()
+#print(tabulate(page1_df))
 
+all_names = []
+for name in page1_df['name']:
+    all_names.append(name)
 
-#x = datetime.datetime(2018, 9, 15, 12, 45, 35)
-#print(page1_df.dtypes())
-#print(type(page1_df['strengths'].tolist()))
-#print(tabulate(page1_df.filter(len(page1_df[strengths > 3].tolist())>3)))
+print(all_names)
 
 
 
