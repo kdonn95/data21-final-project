ab854e2d221c37cc02ccabb5e9115b9127b02aec
Tom <tomwoodbridge32@gmail.com>
2021-06-11 16:53:37 +0100

    merge with csv-test-patch

--- unittests/test_text_file_pipeline.py
+++ unittests/test_text_file_pipeline.py
@@ -1,10 +1,17 @@
 from app.classes.text_file_pipeline import TextFilePipeline
+import pandas as pd
 
-text_pl = TextFilePipeline("Data21Final")
-
+pipeline = TextFilePipeline('Data21Final')
+bucket_name = "data21-final-project"
+key = "Talent/Sparta Day 10 January 2019.txt"
 
 def test_get_txt_file():
-    bucket_name = "data21-final-project"
-    assert type(text_pl.get_txt_file_key_list(bucket_name)) is list
-    assert text_pl.get_txt_file_key_list(bucket_name) != []
-    assert len(text_pl.get_txt_file_key_list(bucket_name)) == 152
+    assert isinstance(pipeline.get_txt_file_key_list(bucket_name), list)
+    assert pipeline.get_txt_file_key_list(bucket_name) != []
+    assert len(pipeline.get_txt_file_key_list(bucket_name)) == 152
+
+def test_text_to_dataframe():
+    result = pipeline.text_to_dataframe(bucket_name, key)
+    assert isinstance(result, pd.DataFrame)
+
+
