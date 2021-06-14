b69011e68063365bad11e98435c5facf1ed1b811
Tom <tomwoodbridge32@gmail.com>
2021-06-14 15:31:36 +0100

    All tests pass individually. 2 fail when using pytest -v

--- unittests/test_text_file_pipeline.py
+++ unittests/test_text_file_pipeline.py
@@ -1,16 +1,17 @@
 from app.classes.text_file_pipeline import TextFilePipeline
 from app.classes.db.db_session import global_init
 from app.classes.get_config import GetConfig
+from app.main import engine
 import pandas as pd
 from datetime import datetime
 
-config = GetConfig()
-conn_str = (
-            f'mssql+pyodbc://{config.user}:{config.password}' +
-            f'@{config.server}/master?driver={config.driver}'
-            )
+# config = GetConfig()
+# conn_str = (
+#             f'mssql+pyodbc://{config.user}:{config.password}' +
+#             f'@{config.server}/master?driver={config.driver}'
+#             )
 
-engine = global_init(conn_str, config.database, 'NORMAL')
+# engine = global_init(conn_str, config.database, 'NORMAL')
 pipeline = TextFilePipeline(engine, "DEBUG")
 bucket_name = "data21-final-project"
 key = "Talent/Sparta Day 10 January 2019.txt"
@@ -20,11 +21,8 @@ def test_get_txt_file():
     assert pipeline.get_txt_file_key_list(bucket_name) != []
     assert len(pipeline.get_txt_file_key_list(bucket_name)) == 152
 
-engine2 = global_init(conn_str, config.database, 'NORMAL')
-pipeline2 = TextFilePipeline(engine2, "DEBUG")
-
 def test_text_to_dataframe():
-    result = pipeline2.text_to_dataframe(bucket_name, key)
+    result = pipeline.text_to_dataframe(bucket_name, key)
     assert isinstance(result, pd.DataFrame)
     date = datetime.strptime('10-01-2019', '%d-%m-%Y')
     date = date.strftime('%Y-%m-%d')
@@ -41,11 +39,8 @@ def test_text_to_dataframe():
     mock_result = pd.DataFrame(data=mock_data)
     assert result.iloc[2].equals(mock_result.iloc[2])
 
-engine3 = global_init(conn_str, config.database, 'NORMAL')
-pipeline3 = TextFilePipeline(engine3, "DEBUG")
-
 def test_transform_string_to_int():
-    df = pipeline3.text_to_dataframe(bucket_name, key)
-    result = pipeline3.transform_string_to_int(df, ['psychometrics', 'psychometrics_max',
+    df = pipeline.text_to_dataframe(bucket_name, key)
+    result = pipeline.transform_string_to_int(df, ['psychometrics', 'psychometrics_max',
                                         'presentation', 'presentation_max'])
     assert isinstance(result.iloc[2]['presentation'], int)
