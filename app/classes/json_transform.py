a557171fd34603e58a53831d52bcdb0f8017523a
EBlundellSparta <EBlundellSparta@spartaglobal.com>
2021-06-14 10:33:13 +0100

    json_transform

--- app/classes/json_transform.py
+++ app/classes/json_transform.py
@@ -43,43 +43,32 @@ all_details = {'name': [],
                'finance_support': [],
                'result': [],
                'course_interest': []
-}
+               }
 
-tech_list = []
-strength_list = []
-weakness_list = []
-
+# Cleans each name and then adds to empty dictionary (which will be turned to dataframe later)
 for name in page1_df['name']:
     new_name = jt.clean_text(name)
     all_details['name'].append(new_name)
 
+# Converts each date to correct format and then adds to dictionary
 for date in page1_df['date']:
     new_date = jt.convert_date(date)
     all_details['date'].append(new_date)
 
+# Adds the tech scores, strengths and weaknesses to dictionary
 for tech_score in page1_df['tech_self_score']:
     if tech_score == 0:
         all_details['tech_self_score'].append({})
     else:
         all_details['tech_self_score'].append(tech_score)
 
-for tech_score in all_details['tech_self_score']:
-    for tech in tech_score.keys():
-        if tech not in tech_list:
-            tech_list.append(tech)
-
 for strength in page1_df['strengths']:
     all_details['strengths'].append(strength)
-    for i in strength:
-        if i not in strength_list:
-            strength_list.append(i)
 
 for weakness in page1_df['weaknesses']:
     all_details['weaknesses'].append(weakness)
-    for j in weakness:
-        if j not in weakness_list:
-            weakness_list.append(j)
 
+# Converts self development, geo-flexible, financial support self and result to a boolean and adds to dictionary
 for self_dev in page1_df['self_development']:
     self_dev = jt.to_bool(self_dev)
     all_details['self_dev'].append(self_dev)
@@ -96,12 +85,9 @@ for result in page1_df['result']:
     result = jt.to_bool(result)
     all_details['result'].append(result)
 
+# Adds course onto dictionary
 for course in page1_df['course_interest']:
     all_details['course_interest'].append(course)
 
-
-df = pd.DataFrame(all_details)
-
-print(sorted(tech_list))
-print(sorted(strength_list))
-print(sorted(weakness_list))
+# Turns all details from dictionary created into a dataframe
+assessment_df = pd.DataFrame(all_details)
