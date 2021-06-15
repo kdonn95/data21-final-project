e5437b40e0c70caa6f66dd3f743b52e58834e197
Lewis <ltwelftree@spartaglobal.com>
2021-06-15 15:55:02 +0100

    added some sql checking under json load

--- app/classes/json_load.py
+++ app/classes/json_load.py
@@ -1,36 +1,25 @@
-<<<<<<< HEAD
-import json
-from app.classes.json_extract import JsonExtract
-from tabulate import tabulate
-import datetime
-import pandas as pd
-from sqlalchemy import orm
 from app.classes.json_transform import JsonTransform
+# import orm
+from app.classes.logger import Logger
+from sqlalchemy.orm import sessionmaker
 
 
-class JsonLoad:
-    def __init__(self, engine):
-=======
-from json_transform import JsonTransform
-import orm
-
-class JsonLoad:
-    def init(self, engine):
->>>>>>> 8199deccfdf84675e82113e0cf8dee9f353e56cb
+class JsonLoad(Logger):
+    def init(self, engine, logging_level):
+        # Initialise logging
+        Logger.init(self, logging_level)
         # Setting up connection to sql server.
         self.engine = engine
-        factory = orm.sessionmaker(bind=self.engine)
+        factory = sessionmaker(bind=self.engine)
         self.session = factory()
         self.session.expire_on_commit = False
 
-
     def candidate_cols(self):
-        return self.engine.execute(f"""SELECT candidate_name FROM candidate""").fetchall()
+        return self.engine.execute(f"""SELECT candidate_name FROM 
+        candidate""").fetchall()
 
     def check_candidate_exists(self, name):
-<<<<<<< HEAD
-        return self.engine.execute(f"SELECT candidate_name FROM candidate WHERE candidate_name = /'john smith'").fetchone()
-
-=======
-        return self.engine.execute(f'SELECT candidate_name FROM candidate WHERE candidate_name = "john smith"').fetchall()
->>>>>>> 8199deccfdf84675e82113e0cf8dee9f353e56cb
+        self.log_print("Checking if candidate exists", "INFO")
+        x = self.engine.execute(f"SELECT * FROM candidate WHERE "
+                                f"candidate_name = '{name}'").fetchall()
+        self.log_print(x, "INFO")
