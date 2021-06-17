b46cd388f1d3f4eee4be0aeebbf5823953a98264
AlexlwAstro <alexander.lw@me.com>
2021-06-17 09:13:36 +0100

    some last unit tests, update gitignore to include my code dump

--- unittests/test_academy_csv_transform.py
+++ unittests/test_academy_csv_transform.py
@@ -1,11 +1,14 @@
-from app.classes.csv_data_transform_erd import courses_table, scores_table, candidates_table
+from app.classes.csv_data_transform_erd import TransformCSVdataFrames
+from app.classes.transform_applicants_csv import candidate_df
 import pandas as pd
 import unittest
 
-course_info_df = courses_table
-scores_df = scores_table
-candidate_info_df = candidates_table
+#course_info_df = courses_table
+#scores_df = scores_table
+#candidate_info_df = candidates_table
 
+scores_df, course_info_df = TransformCSVdataFrames.academy_csv_scores_and_course_dfs_setup()
+candidate_info_dfe = TransformCSVdataFrames.talent_csv_scores_and_course_dfs_setup()
 
 class DataFrameTests(unittest.TestCase):
 
