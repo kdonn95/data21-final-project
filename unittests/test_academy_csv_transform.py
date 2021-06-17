3a5c49c422ebd53767cc7fbada61558d6f3c29d2
AlexlwAstro <alexander.lw@me.com>
2021-06-17 09:20:41 +0100

    editing academy unit test merge conflict

--- unittests/test_academy_csv_transform.py
+++ unittests/test_academy_csv_transform.py
@@ -1,4 +1,5 @@
 from app.classes.csv_data_transform_erd import TransformCSVdataFrames
+from app.classes.boto3_csv_load_pd import GetS3CSVinfo
 from app.classes.transform_applicants_csv import candidate_df
 import pandas as pd
 import unittest
@@ -9,7 +10,7 @@ academy_csv_df_dict = academy_csv_info_getter.create_dict_of_csv_pd_dataframes()
 talent_csv_df_dict = talent_csv_info_getter.create_dict_of_csv_pd_dataframes()
 
 scores_df, course_info_df = TransformCSVdataFrames.academy_csv_scores_and_course_dfs_setup(academy_csv_df_dict)
-candidate_info_dfe = TransformCSVdataFrames.talent_csv_scores_and_course_dfs_setup()
+candidate_info_df = candidate_df
 
 
 class DataFrameTests(unittest.TestCase):
@@ -17,7 +18,7 @@ class DataFrameTests(unittest.TestCase):
     def test_is_dataframe(self):
         self.assertTrue(type(course_info_df) == pd.DataFrame)
         self.assertTrue(type(scores_df) == pd.DataFrame)
-        # self.assertTrue(type(candidate_info_df) == pd.DataFrame)
+        self.assertTrue(type(candidate_info_df) == pd.DataFrame)
 
     def test_shape(self):
         self.assertEqual(course_info_df.shape, (36, 4))
